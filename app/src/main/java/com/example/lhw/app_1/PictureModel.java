945e02ac5fabed1aad490c905e73e855453916fe
[kyk] <[dbsrb992@gmail.com]>
2019-02-12 17:27:30 +0900

    PictureModel.java aws url 수정

---
+++ app/src/main/java/com/example/lhw/app_1/PictureModel.java
@@ -0,0 +1,14 @@
+package com.example.lhw.app_1;
+
+import java.io.Serializable;
+
+public class PictureModel implements Serializable {
+
+    final static String BASE_URL = "http://**.***.***.***/*******/";
+
+    String picture_idx;
+    String product_idx;
+    String user_idx;
+    String picture_name;
+    String picture_path;
+}
